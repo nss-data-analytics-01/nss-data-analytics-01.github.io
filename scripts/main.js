2b7cb7e2453c5aca173ce4a801811bf7fb9005d2
Andy Collins <askingalot@gmail.com>
2019-12-08 17:30:46 -0600

    make site more mobile friendly

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,72 @@
 // initialize the tool-tip plugin for Bootstrap4
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip();
 
+  $("#year").html(new Date().getFullYear());
+
+  $(document).on("click", function() {
+    $(".collapse").collapse("hide");
+  });
+});
 
 $.ajax({
   url: "data/cohort.json"
-}).done(cohortMembers)
-  .fail(function (error) {
+})
+  .done(cohortMembers)
+  .fail(function(error) {
     console.log("error", error);
   });
 
 function cohortMembers(list) {
   let data = list.cohort;
-  data.forEach(function (item) {
-    let studentContact = `<div class="studentContact">`
+  data.forEach(function(item) {
+    let studentContact = `<div class="studentContact">`;
     //if student doesn't have a portfolio site then don't display the icon
     if (item.portfolio != null) {
-
       studentContact += `<a href=${item.portfolio} target="_blank">
       <i class="fas fa-globe fa-2x contactIcons"></i>
-      </a>`
+      </a>`;
     }
     //if student doesn't have a github site then don't display the icon
     if (item.github != null) {
-
       studentContact += `<a href=${item.github} target="_blank">
       <i class="fab fa-github fa-2x contactIcons"></i>
-      </a>`
+      </a>`;
     }
     //if student doesn't have a linkedin site then don't display the icon
     if (item.linkedIn != null) {
-
       studentContact += `<a href=${item.linkedIn} target="_blank">
       <i class="fab fa-linkedin fa-2x contactIcons"></i>
-      </a>`
+      </a>`;
     }
     //if student doesn't have an email then don't display the icon
     if (item.email != null) {
-
       studentContact += `<a href=mailto:${item.email}>
               <i class="fas fa-envelope fa-2x contactIcons"></i>
-            </a>`
+            </a>`;
     }
-    studentContact += `</div>`
+    studentContact += `</div>`;
 
     let studentInfo = `<div class="col-md-3 cohortMems">
           <img class="card-img-top" src="${item.proImg}" alt="${item.firstName} ${item.lastName}" data-toggle="modal" data-target="#cohortMember${item.id}" style="cursor:pointer;">
           <div class="card-body">
-            <h4 class="card-title title-font">${item.firstName} ${item.lastName}</h4>`
+            <h4 class="card-title title-font">${item.firstName} ${item.lastName}</h4>`;
     //if student didn't provide a reelthemin quote then nothing is displayed
     if (item.reelThemIn != null) {
-      studentInfo += `<p class="card-text">${item.reelThemIn}</p>`
+      studentInfo += `<p class="card-text">${item.reelThemIn}</p>`;
     }
-    studentInfo += studentContact
+    studentInfo += studentContact;
 
     //if a student doesn't have a bio, then the learn more button doesn't appear and a modal isn't created
-    if(item.bio != null){
-
-    studentInfo += `
+    if (item.bio != null) {
+      studentInfo += `
             <center><button type="button" class="btn btn-outline-primary title-font bottom" data-toggle="modal" data-target="#cohortMember${item.id}">
            Learn More!
           </button></center>
           </div>
-        </div>`
-    //modal info
-    studentInfo +=`
+        </div>`;
+      //modal info
+      studentInfo += `
         <div class="modal fade" id="cohortMember${item.id}" tabindex="-1" role="dialog" aria-labelledby="cohortMember${item.id}Label" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
@@ -78,12 +79,11 @@ function cohortMembers(list) {
             <div class="modal-body">
             <center><img src="${item.funImg}" /></center><br>
 
-            `
+            `;
 
-    studentInfo += studentContact
+      studentInfo += studentContact;
 
-
-    studentInfo += `
+      studentInfo += `
       
     ${item.bio}
     </div>
@@ -98,37 +98,46 @@ function cohortMembers(list) {
       studentInfo += `
       </div>
         </div>
-        `
+        `;
     }
     document.getElementById("cohort").innerHTML += studentInfo;
-
   });
-};
+}
 //checks to see if url string is empty, if not, creates specified image
 function createLink(urlString, img, mail) {
-  let link = urlString !== '' ? `< a href = "${urlString}" target = "_blank" > <img src="/images/${img}.png"></a>` : '<!-- -->';
-  return link
-};
+  let link =
+    urlString !== ""
+      ? `< a href = "${urlString}" target = "_blank" > <img src="/images/${img}.png"></a>`
+      : "<!-- -->";
+  return link;
+}
 
 function createMailto(urlString, img) {
-  let link = urlString !== '' ? `< a href = "mailto:${urlString}" target = "_blank" > <img src="/images/${img}.png"></a>` : '<!-- -->'
-  return link
+  let link =
+    urlString !== ""
+      ? `< a href = "mailto:${urlString}" target = "_blank" > <img src="/images/${img}.png"></a>`
+      : "<!-- -->";
+  return link;
 }
 
 $.ajax({
   url: "data/techs.json"
-}).done(techs)
-  .fail(function (error) {
+})
+  .done(techs)
+  .fail(function(error) {
     console.log("error", error);
   });
 
 function techs(list) {
   let data = list.techs;
-  data.forEach(function (item) {
-    document.getElementById("techs").innerHTML +=
-      `<div class="col-sm-2 technologies">
-         <center><a href="${item.link}" target="_blank"><img class="techs" src="${item.image}" alt="${item.name}" data-toggle="tooltip" data-placement="top" title="${item.name}"></a><br>
+  data.forEach(function(item) {
+    document.getElementById(
+      "techs"
+    ).innerHTML += `<div class="col-sm-2 technologies">
+         <center>
+         <a href="${item.link}" target="_blank"><img class="techs" src="${item.image}" alt="${item.name}" data-toggle="tooltip" data-placement="top" title="${item.name}"></a>
+         <br>
          </center>
       </div>`;
   });
-};
+}
